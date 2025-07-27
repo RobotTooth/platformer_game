39fc3fe86704da1bdfe581fbc8ead043b14b5146
RobotTooth <bigmeys@hotmail.co.uk>
2025-07-27 23:40:52 +0100

    minor change to the html tags and Play class added and rendered

---
+++ scrpit.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+class Player {
+    constructor(x, y, width, height, velocity) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.velocity = velocity;
+    }
+
+    render() {
+        ctx.fillStyle = '#ff00ff';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+
+    }
+}
+
+const player = new Player(10, 10, 10, 10, 10);
+player.render();
